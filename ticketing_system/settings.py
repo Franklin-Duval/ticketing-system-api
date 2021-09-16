dc85474bb0d710a88c33d84f28c843285fd369f4
Talom_Franklin <franklinfrost14@gmail.com>
2021-09-16 07:57:05 +0100

    feat: add STATIC_ROOT in settings file

--- ticketing_system/settings.py
+++ ticketing_system/settings.py
@@ -162,6 +162,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = 'joe@gmail.com' #replace your email here
